77f79c4bee0abb43e8447e9f79f266671ef88e8b
Tyler Davis <tgfdavis98@gmail.com>
2024-07-29 13:23:33 +0100

    added in booking database logic, calender availablity, fixed some bugs

--- Backend/DatabaseStore.cs
+++ Backend/DatabaseStore.cs
@@ -1,9 +1,13 @@
+using PartyBookingClient.Components.Pages;
+
 namespace PartyBookingClient.Backend;
 
 public class DatabaseStore
 {
     private Dictionary<string, string> _userStore = new Dictionary<string, string>();
-
+    private Dictionary<string, Signup.SignupModel> _profiles = new Dictionary<string, Signup.SignupModel>();
+    private Dictionary<string, BookingManagement.Booking[]> _bookings = new Dictionary<string, BookingManagement.Booking[]>();
+    
     private string _currentlyLoggedInUser = string.Empty;
     
     public DatabaseStore()
@@ -30,9 +34,53 @@ public class DatabaseStore
     public void Login(string username) => _currentlyLoggedInUser = username;
     public bool IsAdmin => _currentlyLoggedInUser == "admin@playland.com";
     public bool IsLoggedIn => !string.IsNullOrEmpty(_currentlyLoggedInUser);
+    public string? Username => _profiles.GetValueOrDefault(_currentlyLoggedInUser)?.Name;
+
+    public BookingManagement.Booking[] GetAllBookings()
+    {
+        var bookings = _bookings.Values.ToArray();
+
+        var list = new List<BookingManagement.Booking>();
+
+        foreach (var booking in bookings)
+        {
+            list.AddRange(booking);
+        }
+
+        return list.ToArray();
+    }
+
+    public BookingManagement.Booking[]? UserBookings => _bookings.GetValueOrDefault(_currentlyLoggedInUser);
 
     public void AddUser(string username, string password)
     {
         _userStore.Add(username, password);
+    }
+
+    public void AddBooking(BookingManagement.Booking booking)
+    {
+        var currentBookings = _bookings.GetValueOrDefault(_currentlyLoggedInUser);
+
+        BookingManagement.Booking[] bookings;
+        
+        if (currentBookings is null)
+        {
+            bookings = new[] { booking };
+        }
+        else
+        {
+            var listBookings = currentBookings.ToList();
+            
+            listBookings.Add(booking);
+
+            bookings = listBookings.ToArray();
+        }
+
+        _bookings[_currentlyLoggedInUser] = bookings;
+    }
+
+    public void AddProfile(Signup.SignupModel model)
+    {
+        _profiles[_currentlyLoggedInUser] = model;
     }
 }
